014f91546d2d36ec5f02ad431d9d823a3ef78fe8
kasra <kasraam385@gmail.com>
2026-06-03 13:24:05 +0330

    more products added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ import datas from "../db.json";
 import Image from "next/image";
 import { link } from "fs";
 import { Separator } from "@/components/ui/separator";
+import { toPersianDigits } from "@/lib/to-persian-digits";
 
 export default function Home() {
   return (
@@ -65,8 +66,8 @@ export default function Home() {
             {datas.products.slice(0, 8).map((item) => (
               <li key={item.id}>
                 <Link href={`/market/${item.id}`}>
-                  <Card className="">
-                    <CardHeader className="bg-accent mx-4 p-0">
+                  <Card className="hover:bg-accent/1 duration-100">
+                    <CardHeader className="bg-accent/70 mx-4 p-0">
                       <Image
                         src={"/"}
                         alt={item.name}
@@ -108,6 +109,99 @@ export default function Home() {
               </li>
             ))}
           </ul>
+        </div>
+      </div>
+
+      <div className="mt-7">
+        <div className="bg-chart-1 rounded-xl p-10 grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="flex flex-col gap-6">
+            <h1 className="text-3xl font-bold text-white">محصولات بیشتر ما</h1>
+            <div className="relative w-full h-100 bg-accent rounded-xl">
+              <Image src={"/"} alt="bg" fill />
+              <Button
+                asChild
+                variant={"default"}
+                size={"lg"}
+                className="absolute text-xl bottom-3 right-3 py-2 h-fit"
+              >
+                <Link className="flex flex-col items-start" href={`/`}>
+                  <span className="font-semibold block">لبنیات</span>
+                  {toPersianDigits(
+                    String(
+                      datas.categories.filter((item) => item.slug === "bakery")
+                        .length,
+                    ),
+                  )}{" "}
+                  محصول
+                </Link>
+              </Button>
+            </div>
+          </div>
+
+          <div className="grid col-span-1 md:grid-cols-2 gap-6">
+            <div className="md:col-span-2 relative md:h-auto h-100 bg-accent rounded-xl">
+              <Image src={`/`} alt="img" fill />
+              <Button
+                asChild
+                variant={"default"}
+                size={"lg"}
+                className="absolute text-xl bottom-3 right-3 py-2 h-fit"
+              >
+                <Link className="flex flex-col items-start" href={`/`}>
+                  <span className="font-semibold block">لبنیات</span>
+                  {toPersianDigits(
+                    String(
+                      datas.categories.filter((item) => item.slug === "bakery")
+                        .length,
+                    ),
+                  )}{" "}
+                  محصول
+                </Link>
+              </Button>
+            </div>
+
+            <div className="relative bg-accent  md:h-auto h-100 rounded-xl">
+              <Image src={`/`} alt="img" fill />
+              <Button
+                asChild
+                variant={"default"}
+                size={"lg"}
+                className="absolute text-xl bottom-3 right-3 py-2 h-fit"
+              >
+                <Link className="flex flex-col items-start" href={`/`}>
+                  <span className="font-semibold block">لبنیات</span>
+                  {toPersianDigits(
+                    String(
+                      datas.categories.filter((item) => item.slug === "bakery")
+                        .length,
+                    ),
+                  )}{" "}
+                  محصول
+                </Link>
+              </Button>
+            </div>
+
+            <div className="relative md:h-auto h-100 bg-accent rounded-xl">
+              <Image src={`/`} alt="img" fill />
+              <Button
+                asChild
+                variant={"default"}
+                size={"lg"}
+                className="absolute text-xl bottom-3 right-3 py-2 h-fit"
+              >
+                <Link className="flex flex-col items-start" href={`/`}>
+                  <span className="font-semibold block">لبنیات</span>
+                  {toPersianDigits(
+                    String(
+                      datas.categories.filter((item) => item.slug === "bakery")
+                        .length,
+                    ),
+                  )}{" "}
+                  محصول
+                </Link>
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
