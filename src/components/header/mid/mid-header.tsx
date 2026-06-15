d4e033b84bce7b9dc7a3a6fada0000ae671e90f7
kasra <kasraam385@gmail.com>
2026-06-15 13:34:57 +0330

    change logo ui

--- src/components/header/mid/mid-header.tsx
+++ src/components/header/mid/mid-header.tsx
@@ -9,13 +9,13 @@ export default function MidHeader() {
     <div className="bg-primary-foreground">
       <div className="max-w-7xl mx-auto gap-1 pl-2 pr-4 py-3 flex items-center justify-between">
         <Link
-          className={`font-bold text-nowrap text-2xl ${nastaliq_font.className}`}
+          className={`font-bold text-shadow-xs text-shadow-black/90 hover:text-[#5a5cc0] duration-200 text-nowrap text-2xl text-[#5a5cc0] md:text-primary ${nastaliq_font.className}`}
           href={`/`}
         >
           مبلغ حسینی
         </Link>
 
-        <HeaderInputs groupClassName="md:flex hidden w-64"/>
+        <HeaderInputs groupClassName="md:flex hidden w-64" />
 
         <HeaderNavDesktop />
         <HeaderNavMobile />
