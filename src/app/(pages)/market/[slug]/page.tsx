f5b62dd97e60c9b6f4b50f6366cbd7c2f30c4a10
ksr <kasraam385@gmail.com>
2026-06-24 01:52:18 +0330

    show filterd by category

--- src/app/(pages)/market/[slug]/page.tsx
+++ src/app/(pages)/market/[slug]/page.tsx
@@ -1,4 +1,7 @@
 import { getProducts } from "@/actions/products";
+import DesktopFilterGrid from "@/components/market/desktop-filter-grid";
+import DesktopSortFilter from "@/components/market/desktop-sort-filter";
+import MobileFilterGrid from "@/components/market/mobile-filter-grid";
 import ProductsGrid from "@/components/market/products-grid";
 
 export default async function CategoryFiltered({
@@ -11,10 +14,27 @@ export default async function CategoryFiltered({
   if (!result_of_products.success) return <div>no fetch can I</div>;
 
   const { slug } = await params;
+
+  const filtered_products = result_of_products.data.filter((product) => {
+    return product.category_slug === slug;
+  });
   return (
     <div>
       My Post: {slug}
-      <ProductsGrid products={result_of_products.data} />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-2 mt-7">
+        {/* filters for desktop */}
+        <DesktopFilterGrid />
+
+        <div className="col-span-1 md:col-span-9">
+          <DesktopSortFilter products_length={result_of_products.data.length} />
+
+          {/* mobile view filtering */}
+          <MobileFilterGrid />
+
+          {/* products */}
+          <ProductsGrid products={filtered_products} />
+        </div>
+      </div>
     </div>
   );
 }
