a34e0e63c647b0bf91c3b152be78b4af6e709a6b
kasra <kasraam385@gmail.com>
2026-07-17 20:10:49 +0330

    added pages and layout to profile

---
+++ src/app/(pages)/profile/wishlists/page.tsx
@@ -0,0 +1,3 @@
+export default function WishLists() {
+  return <div></div>;
+}
---
+++ src/components/profile/navigation.tsx
@@ -0,0 +1,10 @@
+import { Card, CardContent, CardHeader } from "../ui/card";
+
+export default function ProfNavigation() {
+  return (
+    <Card className="col-span-12 order-2 lg:order-1 lg:col-span-3">
+      <CardHeader></CardHeader>
+      <CardContent></CardContent>
+    </Card>
+  );
+}
