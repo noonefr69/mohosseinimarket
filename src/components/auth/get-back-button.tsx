0900f9e6fa82616838e4a689d32c3661004371d9
kasra <kasraam385@gmail.com>
2026-07-19 11:40:18 +0330

    get back button edited

--- src/components/auth/get-back-button.tsx
+++ src/components/auth/get-back-button.tsx
@@ -1,16 +1,42 @@
-"use client"
+"use client";
 
 import { useRouter } from "next/navigation";
 import { Button } from "../ui/button";
 
-export default function GetBackButton() {
-  const router = useRouter()
+export default function GetBackButton({
+  className,
+  variant,
+  size,
+}: {
+  className?: string;
+  variant?:
+    | "link"
+    | "default"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "destructive"
+    | null
+    | undefined;
+  size?:
+    | "default"
+    | "xs"
+    | "sm"
+    | "lg"
+    | "icon"
+    | "icon-xs"
+    | "icon-sm"
+    | "icon-lg"
+    | null
+    | undefined;
+}) {
+  const router = useRouter();
 
   return (
     <Button
-      size="icon-lg"
-      variant={"ghost"}
-      className="font-semibold text-lg hover:no-underline flex items-center justify-center scale-125"
+      size={size}
+      variant={variant}
+      className={`font-semibold text-lg hover:no-underline flex items-center justify-center scale-125 ${className} `}
       onClick={() => router.back()}
     >
       ←
