d41005effcf085452deda60042e0a2e15f68922d
kasra <kasraam385@gmail.com>
2026-07-20 19:43:42 +0330

    change favicon and add some metadata

---
+++ src/app/(pages)/about-us/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "درباره ما",
+  description:
+    "مغازه قدیمی و معتبر مبلغ حسینی حالا آنلاین شده | بهترین برندهای لبنی و خشکبار رو با یه کلیک در خونه‌ت داشته باش | ارسال به تمام نقاط تبریز",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
+}
