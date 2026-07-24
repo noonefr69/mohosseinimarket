b49557c33ccd80f976fc967876c5302ad6c3c653
kasra <kasraam385@gmail.com>
2026-07-24 22:39:40 +0330

    find weird bug but can not find solution for now

--- src/app/(pages)/profile/orders/page.tsx
+++ src/app/(pages)/profile/orders/page.tsx
@@ -1,3 +1,15 @@
-export default function Orders() {
-  return <div>ب</div>;
+import { getOrders } from "@/actions/order/get-orders";
+import OrderCard from "@/components/cart-checkout/tabs/order/order-card";
+import { ErrorAll } from "@/components/Error-all";
+
+export default async function Orders() {
+  const result = await getOrders();
+  if (!result.success) return <ErrorAll error_message={result.error} />;
+  return (
+    <div className="grid lg:grid-cols-2 gap-4">
+      {result.data.map((order) => (
+        <OrderCard order={order} key={order._id} />
+      ))}
+    </div>
+  );
 }
